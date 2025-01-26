241d01f9a7e4e61d0ba8c955fd7d79f62380dd4b
Alejandro Villanueva <aavillanueva6@gmail.com>
2025-01-25 21:49:15 -0800

    added first project, removed template projects

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -189,7 +189,7 @@ const about = {
 
 const blog = {
   label: 'Blog',
-  title: 'Writing about design and tech...',
+  title: 'Writing about things I find interesting',
   description: `Read what ${person.name} has been up to recently`,
   // Create new blog posts by adding a new .mdx file to app/blog/posts
   // All posts will be listed on the /blog route
